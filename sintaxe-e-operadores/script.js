4acada46078ebdeb943acffd50ed8ffe4e259845
Fabio de Moura Martins <fabio.mmouras@hotmail.com>
2022-02-24 11:26:43 -0300

    Update script.js

--- sintaxe-e-operadores/script.js
+++ sintaxe-e-operadores/script.js
@@ -1,11 +1,3 @@
-//Crie uma função que recebe dois números como parâmetros.
-//Confira se os números são iguais.
-//Confira se a soma dos números é maior que 10 ou menor que 20.
-//Retorne uma string dizendo "Os números num1 e num2 não/são iguais. Sua soma é soma, que é maior/menor que 10 e maior/menor que 20".
-//Ex:
-//Input: 1, 2
-//Output: Os números 1 e 2 não são iguais. Sua soma é 3, que é menor que 10 e menor que 20.
-
 // Minha resolução
 function compararNumeros(num1, num2) {
   let soma = num1 + num2;
@@ -78,4 +70,4 @@ function criaSegundaFrase(num1, num2) {
   return `Sua soma é ${soma}, que é ${resultado10} que 10 e ${resultado20} que 20`;
 }
 
-console.log(comparaNumeros(1, 1));
+console.log(comparaNumeros());
