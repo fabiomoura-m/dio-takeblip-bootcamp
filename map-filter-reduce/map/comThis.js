739f51f6b28f9c34400ec83a97815a0ee229997b
Fabio de Moura Martins <fabio.mmouras@hotmail.com>
2022-03-08 11:23:18 -0300

    feat: atividade map com this

---
+++ map-filter-reduce/map/comThis.js
@@ -0,0 +1,19 @@
+const maca = {
+  value: 2
+};
+
+const laranja = {
+  value: 3
+};
+
+function mapComThis(array, thisArg) {
+  return array.map(function (item) {
+    return item * this.value;
+  }, thisArg);
+}
+
+const nums = [1, 2, 3];
+
+console.log('this -> maçã', mapComThis(nums, maca));
+
+console.log('this -> laranja', mapComThis(nums, laranja));
