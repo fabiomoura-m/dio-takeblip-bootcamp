7373a147f104c2216480bbeacee8a4690e3e5e73
Fabio de Moura Martins <fabio.mmouras@hotmail.com>
2022-03-14 14:54:41 -0300

    Create dio.sql

---
+++ mysql/dio.sql
@@ -0,0 +1,26 @@
+CREATE TABLE pessoas(
+  id INT NOT NULL PRIMARY KEY AUTOINCREMENT,
+  nome VARCHAR(30) NOT NULL,
+  nascimento DATE
+)
+
+INSERT INTO pessoas (nome, nascimento) VALUES ("Fabio", "1991 11 08")
+INSERT INTO pessoas (nome, nascimento) VALUES ("Felipe", "1995 07 17")
+INSERT INTO pessoas (nome, nascimento) VALUES ("Natalia", "1990 05 22")
+
+SELECT * FROM pessoas
+SELECT nome From pessoas
+
+UPDATE pessoas set nome = "Fabio Moura" WHERE id = 1
+
+DELETE FROM pessoas WHERE id = 1
+ORDER BY nome
+ORDER BY nome DESC
+
+
+ALTER TABLE pessoas ADD genero VARCHAR(1) NOT NULL AFTER nascimento
+INSERT INTO pessoas (genero) VALUE ("F") WHERE id =3
+INSERT INTO pessoas (genero) VALUE ("M") WHERE id =1
+INSERT INTO pessoas (genero) VALUE ("M") WHERE id =2
+
+SELECT COUNT(qtd), genero FROM pessoas GROUP BY genero
